bd4859f8fb87f9a04708b821d544171ac924b44d
Eanda <eandalined@gmail.com>
2021-01-31 20:03:37 +0700

    add tabs on screen home container

--- lib/screens/home/home_container.dart
+++ lib/screens/home/home_container.dart
@@ -6,10 +6,65 @@ class HomeContainerScreen extends StatefulWidget {
 }
 
 class _HomeContainerState extends State<HomeContainerScreen> {
+  var colorTabsText = Colors.black54;
   @override
   Widget build(BuildContext context) {
+    return DefaultTabController(
+        length: 5,
+        child: Scaffold(
+          body: _appBar(),
+        ),
+    );
+  }
+
+  Container _appBar(){
     return Container(
-      child: Text('Home Container'),
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+        color: Colors.white
+      ),
+      child: _tabBar(),
+    );
+  }
+
+  TabBar _tabBar() {
+    return TabBar(
+      indicatorColor: Colors.red,
+      tabs: <Widget>[
+        Tab(
+          child: Container(
+            child: Text('HOME',style: TextStyle(color: colorTabsText, fontSize: 18.0)),
+          ),
+        ),
+        Tab(
+          child: Container(child: Text('GAMES',style: TextStyle(color: colorTabsText, fontSize: 18.0),)
+        ),
+        ),
+        Tab(
+          child: Container(
+            child: Text(
+              'MOVIES',
+              style: TextStyle(color: colorTabsText, fontSize: 18.0),
+            ),
+          ),
+        ),
+        Tab(
+          child: Container(
+            child: Text(
+              'BOOK',
+              style: TextStyle(color: colorTabsText, fontSize: 18.0),
+            ),
+          ),
+        ),
+        Tab(
+          child: Container(
+            child: Text(
+              'MUSIC',
+              style: TextStyle(color: colorTabsText, fontSize: 18.0),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
