4a3c0da074427c9b042bd5c2b6da4e387a8a12b6
Eanda <eandalined@gmail.com>
2021-02-04 23:44:44 +0700

    add

--- lib/shares/models/product.dart
+++ lib/shares/models/product.dart
@@ -1,10 +1,10 @@
 class Product {
-
+  final int id;
   final String name;
   final double price;
   final String ur;
   final String remark;
   final String show;
-  Product(this.name, this.price,this.ur,this.remark,this.show);
+  Product(this.id,this.name, this.price,this.ur,this.remark,this.show);
 
 }
--- lib/static_data/product_detail_list_items.dart
+++ lib/static_data/product_detail_list_items.dart
@@ -1,6 +1,79 @@
 
 import 'package:ecommerces/shares/models/prodoct_detail_items.dart';
+import 'package:ecommerces/shares/models/product.dart';
 
-// static final ProductDetailItems productDetailItems = [
-//
-// ];
+List<Product> products;
+List<ProductDetailItems> prodouct_detail_items = [
+  ProductDetailItems(
+      'T-Short',
+      [
+        Product(1,'Girl Print Crop T-shirt at SHEIN, Category: T-Shirts', 20, 'https://i.pinimg.com/564x/63/f0/27/63f027e738501bb9978c3cbf3e634be4.jpg', 'remark', '1'),
+        Product(2,'ADIDAS CATEGORY ATH M T-SHIRT BLACK', 20, 'https://hip-hop-shop.supremestyle.eu/sub/hip-hop-shop.supremestyle.eu/shop/product/resized/adidas-category-ath-m-t-shirt-black-53063.thumb_600x600.jpg?t=1517486090', 'remark', '1'),
+        Product(3,'FREE T-SHIRT MOCKUP', 20, 'https://mir-s3-cdn-cf.behance.net/project_modules/1400/a4ef8863145083.5aa7815b81dd2.jpg', 'remark', '1'),
+        Product(4,'T-Shirts - GD01 Gildan Fully Fitted Mens Tee Shirt', 20, 'https://lh3.googleusercontent.com/proxy/_urqjnXCSZa7q_jybAzcoQ434xPygqUpe3Rw1NU4O04FNJSNoHvvPmGC31xow4LFlHFkNP3UrVFNSHLqUzsg0kROilaBjZ9h21YhjXdNah_QyRlXuxWtOdb6-kIXG56-s5BfRcGsB-fmLHIU-3DXFpfVnMXhRrWXuNyfqSI1bbKfcgDBOqjG', 'remark', '1'),
+        Product(5,'ShirtScope I Love Category Theory T Shirt I Heart Category Theory Tee', 20, 'https://images-na.ssl-images-amazon.com/images/I/51r1iAfChTL._AC_UX569_.jpg', 'remark', '1'),
+        Product(6,'U Oversized Crew Neck Short-Sleeve T-Shirt', 20, 'https://image.uniqlo.com/UQ/ST3/WesternCommon/imagesgoods/422995/item/goods_53_422995.jpg', 'remark', '1'),
+        Product(7,'Navy Blue & Red Striped Polo Collar T-shirt', 20, 'https://cdn.shopify.com/s/files/1/0310/3765/3128/products/w19urcmp07-navy-1.jpg?v=1583123601', 'remark', '1'),
+        Product(8,'PATRIOTIC TRUTH NAVY', 20, 'https://shoptruth.taproomtshirts.com/wp-content/uploads/sites/9/2020/05/Heather-Navy-Patriotic-Shirt-04-540x540.jpg', 'remark', '1'),
+        Product(9,'Armin Van Buuren - T-shirt Design', 20, 'https://dianastrumin.com/wp-content/uploads/2019/05/Teniska_Armin_White_Preview.jpg', 'remark', '1'),
+        Product(10,'Mint Green Avocado Embroidered T-Shirt', 20, 'https://cf.shopee.ph/file/d6fe2b0e4da9a9f8c9b88754e2177c59', 'remark', '1'),
+      ]
+  ),
+  ProductDetailItems(
+    'Baby Toys',
+    [
+      Product(1,'Round Shape Silicone Push Pop Bubble Fidget Sensory Toy', 20, 's.alicdn.com/@sc01/kf/Heec0b54593e8442d92f6ceb5e98d9cec0.jpg_300x300.jpg', 'remark', '1'),
+      Product(2,'ADIDAS CATEGORY ATH M T-SHIRT BLACK', 20, '', 'remark', '0'),
+      Product(3,'Food Grade Silicone Animal Shaped Baby Chewing Toys Baby Silicone Teether', 1.45, 'https://s.alicdn.com/@sc01/kf/H65e965742f434666b3667a73096349a9H.jpg_300x300.jpg', 'remark', ''),
+      Product(4,'2020 European Patent Food Grade Spider Silicone Baby Teether ', 3.00, 'https://s.alicdn.com/@sc01/kf/HLB1p2B7RpYqK1RjSZLeq6zXppXau.jpg_300x300.jpg', 'remark', 't'),
+      Product(5,'Kids Mats Pattern Design Customized Foam Playmats Non-toxic Indoor Play Mat', 15, 'https://s.alicdn.com/@sc01/kf/HTB1YJ3gXNrvK1RjSszeq6yObFXai.jpg_300x300.jpg', 'remark', 't'),
+      Product(6,'Baby Fruit Feeder Pacifier Silicone Baby Fresh Fruit Food Feeder Pacifier', 20, 'https://s.alicdn.com/@sc01/kf/H1125930aeb8442b69bfd120290985ae97.jpg_300x300.jpg', 'remark', 't'),
+      Product(7,'Baby Teether Toy Ice Cream Ice Cream Teether Baby Molar Stick Toy Baby Food', 15, 'https://s.alicdn.com/@sc01/kf/H87f884cc1b0a4c5a8afb4de24d0a59c1a.jpg_300x300.jpg', 'remark', 't'),
+      Product(8,'Baby Folding Mat Anti Allergy Baby Rolled Up Play Mat Washable Baby Puzzle Play', 20, 'https://s.alicdn.com/@sc01/kf/HTB1YiGWukCWBuNjy0Faq6xUlXXaz.jpg_300x300.jpg', 'remark', 't'),
+      Product(9,'BBSky Stuffed Animal Baby Soft Plush Hand', 20, 'https://s.alicdn.com/@sc01/kf/HTB1_EvwX._rK1Rjy0Fcq6zEvVXaM.jpg_300x300.jpg', 'remark', 't'),
+      Product(10,'Multi functional fitness rack baby gym', 20, 'https://s.alicdn.com/@sc01/kf/HTB1YSfqaovrK1RjSszfq6xJNVXa0.jpg_300x300.jpg', 'remark', 't'),
+    ]
+  ),
+  ProductDetailItems(
+      'Makeup',
+      [
+        Product(1,'Private Label Moisturizing Nourish Korea', 20, 'https://s.alicdn.com/@sc01/kf/HLB154rAUSzqK1RjSZPxq6A4tVXa5.jpg_300x300.jpg', 'remark', 't'),
+        Product(2,'Half Gallon Lip Gloss Base Mat Gel', 50, 'https://s.alicdn.com/@sc01/kf/Ha689686530d6478eaac6d115ddfd61ffi.jpg_300x300.jpg', '', 't'),
+        Product(3,'Liquid Lipstick Private Label Oem', 1.45, 'https://s.alicdn.com/@sc01/kf/Hb8245b3989ae4152a2daf5a6f6235eab2.jpg_300x300.jpg', 'remark', 't'),
+        Product(4,'Mixed Glitter Nice Looking Valentine', 3.00, 'https://s.alicdn.com/@sc01/kf/H3ca9ca8b5101406596d29b10c7f3e05ek.jpg_300x300.jpg', 'remark', 't'),
+        Product(5,'custom private label empty cosmetic case 6 colours packaging eyeshadow palette container', 15, 'https://s.alicdn.com/@sc01/kf/HLB1YXT4TAvoK1RjSZFwq6AiCFXaL.jpg_300x300.jpg', 'remark', 't'),
+        Product(6,'Smitten Mineral Blush - Glimmer Bronzer', 20, 'https://s.alicdn.com/@sc01/kf/Ude0cb0089c1d400d85156cfb568a0421b.jpg_300x300.jpg', 'remark', 't'),
+        Product(7,'IN STOCK New High Pigment Glitter', 15, 'https://s.alicdn.com/@sc01/kf/H8f3568068fb54857b4514fd1fc5d8effr.png_300x300.png', 'remark', 't'),
+        Product(8,'Private Label 16 Colors Black Empty Eye', 20, 'https://s.alicdn.com/@sc01/kf/H2b13fe5833e44dc4bbad06c251ed1bcfQ.jpg_300x300.jpg', 'remark', 't'),
+        Product(9,'Glitter Eyeshadow UCANBE Twinkle Dust', 20, 'https://s.alicdn.com/@sc01/kf/H4efb1235048d4228962d95ed8ccd0badQ.jpg_300x300.jpg', 'remark', 't'),
+        Product(10,'Gel Eyeliner Wholesale Authentic Music', 20, 'https://s.alicdn.com/@sc01/kf/HLB1vZymSNTpK1RjSZFKq6y2wXXa0.jpg_300x300.jpg', 'remark', 't'),
+      ]
+  ),
+  ProductDetailItems(
+      'Laptops',
+      [
+        Product(1,'High Capacity Battery 10000MAh 14inch IPS Screen Laptop Win10 System Intel Celeron J3455 Quad core 1.6GHz', 20, 'https://s.alicdn.com/@sc01/kf/H4566fe92a818442d8585863260b0ef14m.jpg_220x220.jpg_.webp', 'remark', 't'),
+        Product(2,'Wholesale Athlon series 3150U 15.6 inch Unlock Fingerprints Narrow Bezel Office computer Laptops ', 20, 'https://sc04.alicdn.com/kf/Hfbd24f459b3347548faabd11c909b22d1.jpg_200x200.jpg', '', 't'),
+        Product(3,'Black PC Notebook imported from China ', 1.45, 'https://s.alicdn.com/@sc01/kf/HTB1PedTngaTBuNjSszfq6xgfpXaP.jpg_220x220.jpg_.webp', 'remark', 't'),
+        Product(4,'Factory direct supply cheap new 15.6 inch metal case i7 SSD HDD Backlit keyboard Wifi DVD-RW Slim thin laptop pc free shipping', 3.00, 'https://s.alicdn.com/@sc01/kf/H854332923926470e8c6e6965ca5f2aac5.jpg_220x220.jpg_.webp', 'remark', 't'),
+        Product(5,'New Metal case 15.6 inch IPS intel J3455 8GB RAM laptop computer with backlight ', 15, 'https://s.alicdn.com/@sc01/kf/H4295677df3b847b19d8820c6a0484de5f.jpg_220x220.jpg_.webp', 'remark', 't'),
+        Product(6,'Ultra Slim 14 inch Laptop', 20, 'https://s.alicdn.com/@sc01/kf/HTB1ZQU6OVXXXXbTaXXXq6xXFXXXM.jpg_220x220.jpg_.webp', 'remark', 't'),
+        Product(7,'Cheap slim laptop 14.1 inch win 10 tablet N3350 RAM4GB/6GB /ROM 64GB notebooks laptop computer', 15, 'https://s.alicdn.com/@sc01/kf/HTB1GORxXELrK1Rjy0Fjq6zYXFXaK.jpg_220x220.jpg_.webp', 'remark', 't')
+      ]
+  ),
+  ProductDetailItems(
+      'LED Lighting',
+      [
+        Product(1,'200mm wireless solar portable traffic light', 20, 'https://s.alicdn.com/@sc01/kf/HTB12DNIKrSYBuNjSspfq6AZCpXa3.jpg_300x300.jpg', 'remark', 't'),
+        Product(2,'Wholesale 3.7V 2300mAh Solar Traffic', 20, '', '', 't'),
+        Product(3,'Rechargeable Road Safety Reflective Red Triangle Car Warning Light ', 1.45, 'https://s.alicdn.com/@sc01/kf/Haa69bcd2da704135876a6e17c06e3aafn.jpg_300x300.jpg', 'remark', 't'),
+        Product(4,'flash warning traffic cone light', 3.00, 'https://s.alicdn.com/@sc01/kf/HTB1Y5fRe81D3KVjSZFyq6zuFpXa9.jpg_300x300.jpg', 'remark', 't'),
+        Product(5,'FAA L810 Led Tower Chimney Building Aircraft Obstruction Solar Aviation Warning light ', 15, 'https://s.alicdn.com/@sc01/kf/HTB1n1GKaPzuK1RjSsppq6xz0XXay.jpg_300x300.jpg', 'remark', 't'),
+        Product(6,'Solar Energy Building Flashing Warning Aviation Warning Light DK LED Outdoor 12 Pcs LED Red Yellow Green 90-120/min 24cm*19cm', 20, 'https://s.alicdn.com/@sc01/kf/HLB1hhTzUSzqK1RjSZFLq6An2XXah.jpg_300x300.jpg', 'remark', 't'),
+        Product(7,'Two Colors LED Rechargeable Traffic Baton Traffic Wand', 15, 'https://s.alicdn.com/@sc01/kf/H6d9106fa6ea1414196072de39d5a2f89K.jpg_300x300.jpg', 'remark', 't'),
+        Product(8,'Wide angle led red color mini head lights', 20, 'https://s.alicdn.com/@sc01/kf/Ueef88d6503c04054930030ddfe9d6c70q.jpg_300x300.jpg', 'remark', 't'),
+        Product(9,'Traffic Wand LED Harsh Warning Police Traffic Safety Baton Light Magnet Harsh', 20, 'https://s.alicdn.com/@sc01/kf/HTB1gmPaXjzuK1RjSspeq6ziHVXa4.jpg_300x300.jpg', 'remark', 't'),
+        Product(10,'Replaceable Led Traffic Signal Light', 20, 'https://s.alicdn.com/@sc01/kf/Hb5ad8f24971547e682cee91b1b056e7d8.png_300x300.png', 'remark', 't'),
+      ]
+  ),
+];
